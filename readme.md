cec617afcc26929721cc0ff2ad87c9e6a76de278
def <dennis@felsin9.de>
2015-05-07 20:12:26 +0200

    Standard compliant echo

--- readme.md
+++ readme.md
@@ -6,4 +6,8 @@ This uses the methods from [my blog post](http://hookrace.net/blog/nim-binary-si
 | ---- | ----------:| --------:| -----------:|
 | true |      27 KB |     97 B |       129 B |
 | false|      27 KB |     97 B |       132 B |
-| echo |      31 KB |    356 B |       381 B |
+| echo |      31 KB |    656 B |       800 B |
+
+For quick testing you can compile like this:
+
+    $ nim -d:release --putEnv:file=echo -o:echo c src/framework
--- src/echo.nim
+++ src/echo.nim
@@ -1,5 +1,17 @@
+type StackBuf*[S: static[int], T] = object
+  buf*: array[S, T]
+  pos*: int
+
+proc add[S](s: var StackBuf[S, char], x: char) =
+  s.buf[s.pos] = x
+  inc s.pos
+  if s.pos >= S:
+    stdout.write s.buf, s.pos
+    s.pos = 0
+
 proc main(argc: int, argv: cstringArray): int =
   var
+    buf: StackBuf[4096, char]
     start = 1
     newLine = true
 
@@ -7,10 +19,35 @@ proc main(argc: int, argv: cstringArray): int =
     inc start
     newLine = false
 
-  for i in start .. <argc:
-    stdout.write argv[i], strlen(argv[i])
-    if i < argc - 1:
-      stdout.write " ", 1
+  block outer:
+    for i in start .. <argc:
+      var j = 0
+      while true:
+        case argv[i][j]
+        of '\0': break
+        of '\\':
+          inc j
+          case argv[i][j]
+          of 'a':  buf.add '\a'
+          of 'b':  buf.add '\b'
+          of 'c':  break outer
+          of 'f':  buf.add '\f'
+          of 'n':  buf.add '\l'
+          of 'r':  buf.add '\r'
+          of 't':  buf.add '\t'
+          of 'v':  buf.add '\v'
+          of '\\': buf.add '\\'
+          else:
+            buf.add '\\'
+            buf.add argv[i][j]
+        else:
+          buf.add argv[i][j]
+        inc j
+      if i < argc - 1:
+        buf.add ' '
 
-  if newLine:
-    stdout.write "\n", 1
+    if newLine:
+      buf.add '\l'
+
+  stdout.write buf.buf, buf.pos
+  result = 0
