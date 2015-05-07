7dc317726f982cfe6a57f91b57883e919aa11932
def <dennis@felsin9.de>
2015-05-07 04:36:10 +0200

    Some progress on echo

--- src/echo.nim
+++ src/echo.nim
@@ -1,5 +1,10 @@
-import stdlib
+include stdlib
 
-proc main(argv: cstringArray) {.exportc: "_start".} =
-  stdout.write "Hello\n", 6
-  exit 0
+proc main(argc: int, argv: cstringArray): cint =
+  for i in 1 .. <argc:
+    stdout.write argv[i], strlen(argv[i])
+    if i > argc - 1:
+      stdout.write " ", 1
+  return 1
+
+init()
--- src/stdlib.nim
+++ src/stdlib.nim
@@ -1,12 +1,42 @@
 import syscall
 
+template `+`*[T](p: ptr T, off: int): ptr T =
+  cast[ptr type(p[])](cast[ByteAddress](p) +% off * sizeof(p[]))
+
+template `+=`*[T](p: ptr T, off: int) =
+  p = p + off
+
+template `-`*[T](p: ptr T, off: int): ptr T =
+  cast[ptr type(p[])](cast[ByteAddress](p) -% off * sizeof(p[]))
+
+template `-=`*[T](p: ptr T, off: int) =
+  p = p - off
+
+template `[]`*[T](p: ptr T, off: int): T =
+  (p + off)[]
+
+template `[]=`*[T](p: ptr T, off: int, val: T) =
+  (p + off)[] = val
+
 const
   stdin* = 0
   stdout* = 1
   stderr* = 2
 
-proc exit*(n: clong) {.inline, noReturn.} =
+proc exit*(n: cint) {.inline, noReturn.} =
   discard syscall(EXIT, n)
 
 proc write*(fd: cint, buf: cstring, len: csize): clong {.inline, discardable.} =
   syscall(WRITE, fd, buf, len)
+
+proc strlen*(s: cstring): csize =
+  while s[result] != '\0':
+    inc result
+
+template init* {.dirty.} =
+  proc start*(firstParam: int) {.exportc: "_start".} =
+    var stack: ptr int
+    {.emit: "`stack` = &`firstParam`;".}
+    let argc = stack[1]
+    let argv = cast[cstringArray](stack+2)
+    exit main(argc, argv)
